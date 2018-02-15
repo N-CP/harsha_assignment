46f2d77253b0880b828c3050723fd00086aebe9d
N-CP <nikhil.parab@tesseractspl.com>
2018-02-15 11:20:27 +0530

    modified

--- redshift.py
+++ redshift.py
@@ -3,17 +3,17 @@ import re
 
 def stringManipulation(str): 
   if "CREATE USER" in str:
-      
-    nums = str.split(' ')
-    nums[2]=nums[2].title()+'123'
-    str = ""
+    stmt= str.replace("WITH SYSID","")  
+    nums = stmt.split(' ')
+    nums[6]="'"+nums[2].title()+'123'+"'"
+    stmt = ""
     for num in nums:
         
       # Pick the username (num2) and append 123
       if len(num) != 3:   # Check any Python fucntion to check whether it is number or not
         
-        str = str + " "+ num + " "
-    return str.replace("SYSID", "").replace("WITH", "")
+        stmt = stmt +" " +num
+    return stmt
     
   elif "CREATE GROUP" in str:
       str=re.sub("WITH SYSID\s\d+","",str)
@@ -33,28 +33,23 @@ def stringManipulation(str):
   else: 
     return " " # Logic need to write, either empty string or word 
   
-Read = open('C:\\Users\\admin\\Desktop\\nr.txt')
+Read = open('C:\\Users\\Administrator\\Desktop\\assignment_redshift\\new.txt')
 
 replace = list(map(stringManipulation,Read))
 
-repstr=str(replace).strip('[]')
-repstr=re.sub("\,","",repstr)
-repstr=re.sub('\"',"",repstr)
-
-# Take filter RDD and then write the Python code to push it to RS
 
 conn=psycopg2.connect(dbname= 'dbname', host='host', 
-port= '5439', user= 'user', password= 'pwd')
+port= '5439', user= 'user', password= 'pwd!')
 
 
 cur = conn.cursor()
+for i in replace:
+   print(i)
+   try: 
+      cur.execute(i)
+   except:
+       print(Exception)
+    
+conn.commit()
 
-
-replaceRDD1 = list(map(stringManipulation,ReadRDD))
-
-cur.execute(str(repstr))
-
-
-# Take filter RDD and then write the Python code to push it to RS
-
-print(repstr)
+print(replace[0])
